5038610ac6e522607ea14e97a0d10e685bd12c84
Mark <hendriksen-mark@hotmail.com>
2024-03-11 09:35:12 +0100

    update

--- date time test.py
+++ date time test.py
@@ -1,8 +1,19 @@
 from datetime import datetime, timezone
 
 creation_time = "2024-02-18 19:50:15.000000 +0100\n"
+#creation_time = "2024-02-18 19:50:15.000000\n"
 creation_time_arg1 = creation_time.replace(".", " ").split(" ")#2024-02-18, 19:50:15, 000000000, +0100\n
-creation_time = creation_time_arg1[0] + " " + creation_time_arg1[1] + " " + creation_time_arg1[3].replace("\n", "")#2024-02-18 19:50:15 +0100
-creation_time = datetime.strptime(creation_time, "%Y-%m-%d %H:%M:%S %z").astimezone(timezone.utc).strftime("%Y-%m-%d %H")
+print(len(creation_time_arg1))
+if (len(creation_time_arg1) < 4):
+    creation_time = creation_time_arg1[0] + " " + creation_time_arg1[1].replace("\n", "")#2024-02-18 19:50:15
+    creation_time = datetime.strptime(creation_time, "%Y-%m-%d %H:%M:%S").astimezone(timezone.utc).strftime("%Y-%m-%d %H")#2024-02-18 18
+else:
+    creation_time = creation_time_arg1[0] + " " + creation_time_arg1[1] + " " + creation_time_arg1[3].replace("\n", "")#2024-02-18 19:50:15 +0100
+    creation_time = datetime.strptime(creation_time, "%Y-%m-%d %H:%M:%S %z").astimezone(timezone.utc).strftime("%Y-%m-%d %H")#2024-02-18 18
 
 print(creation_time)
+
+print(datetime.now().strftime("T%H:%M:%S"))
+while True:
+    if (datetime.now().strftime("T%H:%M:%S") == "T23:24:00"):
+        print("true")
--- githubCheck.py
+++ githubCheck.py
@@ -8,12 +8,15 @@ from datetime import datetime, timezone
 
 #url = "https://api.github.com/repos/diyhue/diyhue/branches/master"
 url = "https://api.github.com/repos/hendriksen-mark/diyhue/branches/master"
+#url = "https://api.github.com/repos/diyhue/diyHueUI/releases/latest"
 response = requests.get(url)
 
 if response.status_code == 200:
     device_data = json.loads(response.text)
+    print(device_data["published_at"])
     #publish_time = device_data["commit"]["commit"]["author"]["date"]
-    publish_time = datetime.strptime(device_data["commit"]["commit"]["author"]["date"], "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%d %H")
+    #publish_time = datetime.strptime(device_data["commit"]["commit"]["author"]["date"], "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%d %H")
+    publish_time = datetime.strptime(device_data["published_at"], "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%d %H")
 
 #print(creation_time)
 print(publish_time)
