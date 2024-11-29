b49d081e744d4b5440fb43f4507c0b75edf00054
Mark Hendriksen <hendriksen-mark@hotmail.com>
2024-11-29 14:42:51 +0100

    wled test

---
+++ wled_test/WledDevice.py
@@ -0,0 +1,70 @@
+import WledData
+
+class WledDevice:
+
+    def __init__(self, ip, mdns_name):
+        self.ip = ip
+        self.name = mdns_name.split(".")[0]
+        self.url = 'http://' + self.ip
+        self.ledCount = 0
+        self.mac = None
+        self.segmentCount = 1  # Default number of segments in WLED
+        self.segments = []
+        self.getInitialState()
+
+    def getInitialState(self):
+        self.state = self.getLightState()
+        self.getSegments()
+        self.getLedCount()
+        self.getMacAddr()
+
+    def getLedCount(self):
+        self.ledCount = self.state['info']['leds']['count']
+
+    def getMacAddr(self):
+        self.mac = ':'.join(self.state[
+                            'info']['mac'][i:i+2] for i in range(0, 12, 2))
+
+    def getSegments(self):
+        self.segments = self.state['state']['seg']
+        self.segmentCount = len(self.segments)
+
+    def getLightState(self):
+        data = WledData.wled
+        return data
+
+    def getSegState(self, seg):
+        state = {}
+        data = self.getLightState()['state']
+        seg = data['seg'][seg]
+        state['bri'] = seg['bri']
+        state['on'] = data['on'] # Get on/off at light level
+        state['bri'] = seg['bri']
+        # Weird division by zero when a color is 0
+        r = int(seg['col'][0][0])+1
+        g = int(seg['col'][0][1])+1
+        b = int(seg['col'][0][2])+1
+        state['xy'] = self.convert_rgb_xy(r, g, b)
+        state["colormode"] = "xy"
+        return state
+    
+    def convert_rgb_xy(red, green, blue):
+        red = pow((red + 0.055) / (1.0 + 0.055), 2.4) if red > 0.04045 else red / 12.92
+        green = pow((green + 0.055) / (1.0 + 0.055), 2.4) if green > 0.04045 else green / 12.92
+        blue = pow((blue + 0.055) / (1.0 + 0.055), 2.4) if blue > 0.04045 else blue / 12.92
+
+    #Convert the RGB values to XYZ using the Wide RGB D65 conversion formula The formulas used:
+        X = red * 0.664511 + green * 0.154324 + blue * 0.162028
+        Y = red * 0.283881 + green * 0.668433 + blue * 0.047685
+        Z = red * 0.000088 + green * 0.072310 + blue * 0.986039
+
+    #Calculate the xy values from the XYZ values
+        div = X + Y + Z
+        if div < 0.000001:
+            # set values to zero in case of div by zero
+            x = 0
+            y = 0
+        else:
+            x = X / div
+            y = Y / div
+        return [x, y]
