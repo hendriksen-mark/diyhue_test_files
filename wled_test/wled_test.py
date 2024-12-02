b3ac075e0ae4ccaa13e8eea53793d2d7589f967a
Mark <hendriksen-mark@hotmail.com>
2024-12-03 00:19:22 +0100

    Update wled_test.py

--- wled_test/wled_test.py
+++ wled_test/wled_test.py
@@ -1,15 +1,15 @@
 import logManager
 from scan import scanForLights, load_light, save_lights, bridgeConfig_Light
-import random
+from random import randrange
 from colors import convert_rgb_xy
 from entertainment import run_entertainment
 
 logging = logManager.logger.get_logger(__name__)
 
 def set_light():
-    r = random.randrange(0, 255)#255
-    g = random.randrange(0, 255)#127
-    b = random.randrange(0, 255)#9
+    r = randrange(0, 255)#255
+    g = randrange(0, 255)#127
+    b = randrange(0, 255)#9
     light_nr = 24
     light = bridgeConfig_Light[str(light_nr)]
     light.setV1State({"on": True, "bri": 254, "transitiontime": 4, "xy": convert_rgb_xy(r, g, b), "colormode": "xy"})
@@ -29,6 +29,6 @@ load_light("/lights1.yaml")
 
 #save_lights("/lights2.yaml")
 
-run_entertainment()
+#run_entertainment()
 #set_light()
 #get_light()
