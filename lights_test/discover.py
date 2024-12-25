4e94e3646e28c41daf6857055f42f6d4e5747f33
Mark <hendriksen-mark@hotmail.com>
2024-12-25 14:56:04 +0100

    Update discover.py

--- lights_test/discover.py
+++ lights_test/discover.py
@@ -124,7 +124,7 @@ def scanForLights():  # scan for ESP8266 lights and strips
                         lightIsNew = False
                 elif light["protocol"] in ["govee"]:
                     # Check based on device_id and model
-                    if lightObj.protocol_cfg["device_id"] == light["protocol_cfg"]["device_id"] and lightObj.protocol_cfg["model"] == light["protocol_cfg"]["model"] and lightObj.protocol_cfg["segmentedID"] == light["protocol_cfg"]["segmentedID"]:
+                    if lightObj.protocol_cfg["device_id"] == light["protocol_cfg"]["device_id"] and lightObj.protocol_cfg["sku_model"] == light["protocol_cfg"]["sku_model"] and lightObj.protocol_cfg["segmentedID"] == light["protocol_cfg"]["segmentedID"]:
                         logging.info(f'govee light already present {light["name"]}')
                         lightIsNew = False
         if lightIsNew:
