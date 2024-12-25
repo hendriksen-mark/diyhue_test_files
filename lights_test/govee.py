fbdffc326915efa7e3cae48624c8f8b52686abad
Mark <hendriksen-mark@hotmail.com>
2024-12-25 14:56:07 +0100

    Update govee.py

--- lights_test/govee.py
+++ lights_test/govee.py
@@ -8,6 +8,7 @@ logging = logManager.logger.get_logger(__name__)
 
 API_KEY = ""
 BASE_URL = "https://openapi.api.govee.com/router/api/v1/"
+BASE_TYPE = "devices.capabilities."
 
 def get_headers():
     return {
@@ -27,51 +28,65 @@ def discover(detectedLights):
         device_name = device.get("deviceName", f'{device["sku"]}-{device_id.replace(":","")[10:]}')
         capabilities = []
         for function in device["capabilities"]:
-            capabilities.append(function["instance"])
-        if all(x in capabilities for x in ["powerSwitch", "brightness", "segmentedColorRgb"]):
+            capabilities.append(function["type"])
+        if all(x in capabilities for x in [f"{BASE_TYPE}on_off", f"{BASE_TYPE}segment_color_setting"]):
             model = "LST002"
             for function in device["capabilities"]:
-                if function["instance"] == "segmentedColorRgb":
+                if function["type"] == f"{BASE_TYPE}segment_color_setting":
                     logging.debug(f"Govee: Found {device_name} with {len(function['parameters']['fields'][0]['options'])} segments")
                     for option in function["parameters"]["fields"][0]["options"]:
-                        detectedLights.append({"protocol": "govee", "name": f"{device_name}-seg{option['value']}", "modelid": model, "protocol_cfg": {"device_id": device_id, "model": device["sku"], "segmentedID": option["value"]}})
+                        detectedLights.append({"protocol": "govee", "name": f"{device_name}-seg{option['value']}", "modelid": model, "protocol_cfg": {"device_id": device_id, "sku_model": device["sku"], "segmentedID": option["value"]}})
             continue
-        elif all(x in capabilities for x in ["powerSwitch", "brightness", "colorRgb"]):
+        elif all(x in device["capabilities"] for x in [f"{BASE_TYPE}on_off", f"{BASE_TYPE}color_setting"]):
             model = "LCT015"
-        elif all(x in capabilities for x in ["powerSwitch", "brightness", "colorTem"]):
-            model = "LTW001"
-        elif all(x in capabilities for x in ["powerSwitch", "brightness"]):
-            model = "LWB010"
-        elif all(x in capabilities for x in ["powerSwitch"]):
+        elif all(x in device["capabilities"] for x in [f"{BASE_TYPE}on_off"]):
             model = "LOM010"
-        detectedLights.append({"protocol": "govee", "name": device_name, "modelid": model, "protocol_cfg": {"device_id": device_id, "model": device["sku"]}})
+        detectedLights.append({"protocol": "govee", "name": device_name, "modelid": model, "protocol_cfg": {"device_id": device_id, "sku_model": device["sku"]}})
         logging.debug(f"Govee: Found {device_name} with model {model}")
 
 def set_light(light, data):
     logging.debug(f"Govee: <set_light> invoked! Device ID={light.protocol_cfg['device_id']}")
     device_id = light.protocol_cfg["device_id"]
-    model = light.protocol_cfg["model"]
-    request_data = {"device": device_id, "model": model, "cmd": {"name": "turn", "value": "off" if "on" in data and not data['on'] else "on"}}
+    model = light.protocol_cfg["sku_model"]
+    request_data = {"sku": model, "device": device_id, "capability": {}}
 
     if "bri" in data:
         brightness = data['bri']
-        request_data["cmd"] = {"name": "brightness", "value": brightness}
+        request_data["capability"] = {
+            "type": f"{BASE_TYPE}segment_color_setting",
+            "instance": "segmentedBrightness",
+            "value": {
+                "segment": [light.protocol_cfg.get("segmentedID", 0)]},
+                "brightness": brightness
+        }
 
     if "xy" in data:
-        color = convert_xy(data['xy'][0], data['xy'][1], data.get('bri', 255))
-        request_data["cmd"] = {"name": "color", "value": {"r": color[0], "g": color[1], "b": color[2]}}
+        r,g,b = convert_xy(data['xy'][0], data['xy'][1], data.get('bri', 255))
+        request_data["capability"] = {
+            "type": f"{BASE_TYPE}segment_color_setting",
+            "instance": "segmentedColorRgb",
+            "value": {
+                "segment": [light.protocol_cfg.get("segmentedID", 0)]},
+                "rgb": (((r & 0xFF) << 16) | ((g & 0xFF) << 8) | ((b & 0xFF) << 0))
+        }
 
     if "ct" in data:
-        request_data["cmd"] = {"name": "colorTem", "value": data['ct']}
+        request_data["capability"] = {"name": "colorTem", "value": data['ct']}
 
     if "hue" in data or "sat" in data:
         hue = data.get('hue', 0)
         sat = data.get('sat', 0)
         bri = data.get('bri', 255)
-        color = hsv_to_rgb(hue, sat, bri)
-        request_data["cmd"] = {"name": "color", "value": {"r": color[0], "g": color[1], "b": color[2]}}
+        r,g,b = hsv_to_rgb(hue, sat, bri)
+        request_data["capability"] = {
+            "type": f"{BASE_TYPE}segment_color_setting",
+            "instance": "segmentedColorRgb",
+            "value": {
+                "segment": [light.protocol_cfg.get("segmentedID", 0)]},
+                "rgb": (((r & 0xFF) << 16) | ((g & 0xFF) << 8) | ((b & 0xFF) << 0))
+        }
 
-    response = requests.put(f"{BASE_URL}/device/control", headers=get_headers(), data=json.dumps(request_data))
+    response = requests.put(f"{BASE_URL}/device/control", headers=get_headers(), data=json.dumps({"requestId": "1", "payload": request_data}))
     response.raise_for_status()
 
 def get_light_state(light):
